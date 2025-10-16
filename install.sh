65f3a8a00f4a0e6b2cb7ac4538cbe2538aa5efeb
AirEnjoyer <nicholasmilligan2019@outlook.com>
2025-10-16 12:30:43 -0700

    Updated install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ g++ install.cpp -o install
 mkdir build
 cd build
 ../install mkfat
-../install qclone
+../install clone
 ../install sdltemplate
 ../install template
+../install tobin 
 ../install txt2pdf
