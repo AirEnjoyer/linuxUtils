cea5f21ee40e038c495ca96cd43849bf2138d9a7
AirEnjoyer <nicholasmilligan2019@outlook.com>
2025-10-20 08:58:28 -0700

    Updated clone

--- clone/src/main.cpp
+++ clone/src/main.cpp
@@ -1,22 +1,36 @@
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 #include <string>
 
-void getRepo(std::string &clone){
-    std::cout << "Enter the author name and repo name (eg. sindresorhus/awesome)" << std::endl;
-    std::string repo;
-    std::cin >> repo;
-    clone += repo;
+void getRepo(std::string &clone) {
+  std::cout << "Enter the author name and repo name (eg. sindresorhus/awesome)"
+            << std::endl;
+  std::string repo;
+  std::cin >> repo;
+  clone += repo;
 }
 
 int main(int argc, char *argv[]) {
-    std::string clone = "git clone --depth 1 --branch main --single-branch https://github.com/"; 
-    if (argc > 1){
-        clone += argv[1];
-    } else {
-        getRepo(clone);
-    }
-    system(clone.c_str());
+  std::string clone = "git clone --depth 1 --branch ";
+  // main --single-branch https://github.com/";
+  if (argc == 2) {
+    std::cout << "Using default branch main, add branch after repo to use a "
+                 "different one"
+              << std::endl;
+    clone += "main --single-branch https://github.com/";
+    clone += argv[1];
+  } else if (argc == 3) {
+    clone += argv[2];
+    clone += " --single-branch https://github.com/";
+    clone += argv[1];
+  } else {
+    std::cout << "Usage: clone Author/repoName " << std::endl
+              << "Optionally, do Author/repoName branch to use a branch other "
+                 "than main"
+              << std::endl;
+    exit(1);
+  }
+  system(clone.c_str());
 
-return 0;
+  return 0;
 }
