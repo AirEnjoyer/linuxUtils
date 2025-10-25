42bd922d6c4df2e1bd531aa7317a3cbf27a4ef12
AirEnjoyer <nicholasmilligan2019@outlook.com>
2025-10-25 16:00:19 -0700

    Updated sdltemplate

--- sdltemplate/src/main.cpp
+++ sdltemplate/src/main.cpp
@@ -14,6 +14,75 @@ int main(int argc, char *argv[]) {
       std::cin >> projectName;
     }
   }
+  std::system("mkdir src");
+
+  std::ofstream main("src/main.cpp");
+
+  main << "#include <SDL3/SDL.h>" << "\n"
+       << "#include <iostream>" << "\n"
+       << "\n";
+
+  main << "SDL_Window *CreateWindow(const char *title, int width, int height,"
+       << "\n"
+       << "Uint32 flags){" << "\n"
+       << "SDL_Window *window = SDL_CreateWindow(title, width, height, flags);"
+       << "\n"
+       << "return window;" << "\n"
+       << "}" << "\n"
+       << "\n";
+
+  main << "SDL_Renderer *CreateRenderer(SDL_Window *window){" << "\n"
+       << "SDL_Renderer *renderer= SDL_CreateRenderer(window, nullptr);"
+       << "\n"
+       << "return renderer;" << "\n"
+       << "}" << "\n"
+       << "\n";
+
+  main << "void MainLoop(SDL_Renderer *renderer){" << "\n"
+       << "bool running = true;" << "\n"
+       << "SDL_Event event;" << "\n"
+       << "while (running) {" << "\n"
+       << "while (SDL_PollEvent(&event)) {" << "\n"
+       << "if (event.type == SDL_EVENT_QUIT) {" << "\n"
+       << "running = false;" << "\n"
+       << "}" << "\n"
+       << "}" << "\n";
+
+  main << "SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);" << "\n"
+       << "SDL_RenderClear(renderer);" << "\n"
+       << "SDL_RenderPresent(renderer);"
+          "\n"
+       << "}" << "\n"
+       << "}" << "\n";
+
+  main << "void CleanUp(SDL_Window *window, SDL_Renderer *renderer) {"
+          "\n"
+       << "SDL_DestroyRenderer(renderer);" << "\n"
+       << "SDL_DestroyWindow(window);" << "\n"
+       << "SDL_Quit();" << "\n"
+       << "}" << "\n"
+       << "\n";
+
+  main << "int main (int argc, char *argv[]) {"
+          "\n"
+       << "if  (!SDL_Init(SDL_INIT_VIDEO)) {" << "\n"
+       << "std::cout << SDL_GetError() << std::endl;" << "\n"
+       << "}" << "\n"
+       << "\n"
+       << "SDL_Window *window = CreateWindow(\"window\", 640, 480, "
+          "SDL_WINDOW_RESIZABLE);"
+       << "\n"
+       << "SDL_Renderer *renderer = CreateRenderer(window);" << "\n"
+       << "\n"
+       << "\n"
+       << "MainLoop(renderer);" << "\n"
+       << "CleanUp(window, renderer);" << "\n"
+       << "\n"
+       << "\n"
+       << "return 0;"
+       << "}";
+  main.close();
+
   std::ofstream CMakeLists("CMakeLists.txt");
 
   CMakeLists << "cmake_minimum_required(VERSION 3.7)" << std::endl << std::endl;
@@ -44,9 +113,7 @@ int main(int argc, char *argv[]) {
 
   CMakeLists.close();
   std::system("mkdir build");
-  std::system("mkdir src");
   std::system("mkdir headers");
-  std::system("cp ~/src/main.cpp ./src/main.cpp");
   std::system("cmake -S . -B build/");
 
   return 0;
