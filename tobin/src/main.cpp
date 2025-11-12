027552ad118980baeff4b82e914c90fb4e1ffe6c
nick <nicholasmilligan2019@outlook.com>
2025-11-12 15:33:30 -0800

    Updated tobin

--- tobin/src/main.cpp
+++ tobin/src/main.cpp
@@ -3,20 +3,20 @@
 #include <string>
 
 void toBin(std::string path) {
-  std::string toBin = "cp ";
-  toBin += path;
-  toBin += " /usr/local/bin";
-  std::system(toBin.c_str());
+    std::string toBin = "cp ";
+    toBin += path;
+    toBin += " /usr/bin";
+    std::system(toBin.c_str());
 }
 
 int main(int argc, char *argv[]) {
-  std::string path;
-  if (argc > 1) {
-    path = argv[1];
-  } else {
-    std::cout << "Enter the path to the executable: " << std::flush;
-    std::cin >> path;
-  }
-  toBin(path);
-  return 0;
+    std::string path;
+    if (argc > 1) {
+        path = argv[1];
+    } else {
+        std::cout << "Enter the path to the executable: " << std::flush;
+        std::cin >> path;
+    }
+    toBin(path);
+    return 0;
 }
