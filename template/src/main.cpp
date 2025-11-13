57faf6fc961537f8481aa91f2b4bff907af34c5d
nick <nicholasmilligan2019@outlook.com>
2025-11-12 16:40:40 -0800

    Updated templates to export compile commands

--- template/src/main.cpp
+++ template/src/main.cpp
@@ -4,51 +4,53 @@
 #include <string>
 
 int main(int argc, char *argv[]) {
-  std::string projectName;
-  if (argc > 1) {
-    projectName = argv[1];
-  } else {
-    while (projectName.empty()) {
-      system("clear");
-      std::cout << "Please enter the name for the project" << std::endl;
-      std::cin >> projectName;
+    std::string projectName;
+    if (argc > 1) {
+        projectName = argv[1];
+    } else {
+        while (projectName.empty()) {
+            system("clear");
+            std::cout << "Please enter the name for the project" << std::endl;
+            std::cin >> projectName;
+        }
     }
-  }
-  std::ofstream CMakeLists("CMakeLists.txt");
+    std::ofstream CMakeLists("CMakeLists.txt");
 
-  CMakeLists << "cmake_minimum_required(VERSION 3.25)" << std::endl
-             << std::endl;
+    CMakeLists << "cmake_minimum_required(VERSION 3.25)" << std::endl
+               << std::endl;
 
-  CMakeLists << "project(" << projectName << " C CXX)" << std::endl
-             << std::endl;
+    CMakeLists << "project(" << projectName << " C CXX)" << std::endl
+               << std::endl;
 
-  CMakeLists << "set(CMAKE_CXX_STANDARD 20)" << std::endl;
-  CMakeLists << "set(CMAKE_CXX_STANDARD_REQUIRED ON)" << std::endl;
-  CMakeLists << "set(CMAKE_EXPORT_COMPILE_COMMANDS ON)" << std::endl
-             << std::endl;
+    CMakeLists << "set(CMAKE_CXX_STANDARD 20)" << std::endl;
+    CMakeLists << "set(CMAKE_CXX_STANDARD_REQUIRED ON)" << std::endl;
+    CMakeLists << "set(CMAKE_EXPORT_COMPILE_COMMANDS ON)" << std::endl
+               << std::endl;
 
-  CMakeLists << "set(SOURCE_DIR \"${CMAKE_CURRENT_SOURCE_DIR}/src\")"
-             << std::endl;
-  CMakeLists << "file(GLOB SOURCE_FILES \"${SOURCE_DIR}/*.cpp\")" << std::endl
-             << std::endl;
-  CMakeLists << "add_executable(${PROJECT_NAME} ${SOURCE_FILES})" << std::endl
-             << std::endl;
+    CMakeLists << "set(SOURCE_DIR \"${CMAKE_CURRENT_SOURCE_DIR}/src\")"
+               << std::endl;
+    CMakeLists << "file(GLOB SOURCE_FILES \"${SOURCE_DIR}/*.cpp\")" << std::endl
+               << std::endl;
+    CMakeLists << "add_executable(${PROJECT_NAME} ${SOURCE_FILES})" << std::endl
+               << std::endl;
 
-  CMakeLists << "target_include_directories(${PROJECT_NAME} PUBLIC "
-                "\"${CMAKE_CURRENT_SOURCE_DIR}/include\")";
+    CMakeLists << "target_include_directories(${PROJECT_NAME} PUBLIC "
+                  "\"${CMAKE_CURRENT_SOURCE_DIR}/include\")";
 
-  CMakeLists.close();
-  system("mkdir build");
-  system("mkdir src");
-  system("mkdir include");
+    CMakeLists.close();
+    system("mkdir build");
+    system("mkdir src");
+    system("mkdir include");
 
-  std::ofstream main("src/main.cpp");
-  main << "#include <iostream>" << std::endl << std::endl;
-  main << "int main() {" << std::endl
-       << "int x = 0;" << std::endl
-       << "return 0;" << std::endl
-       << "}" << std::endl;
-  main.close();
+    std::ofstream main("src/main.cpp");
+    main << "#include <iostream>" << std::endl << std::endl;
+    main << "int main() {" << std::endl
+         << "int x = 0;" << std::endl
+         << "return 0;" << std::endl
+         << "}" << std::endl;
+    main.close();
 
-  return 0;
+    std::system("cmake -S . -B build DCMAKE_EXPORT_COMPILE_COMMANDS=ON")
+
+        return 0;
 }
